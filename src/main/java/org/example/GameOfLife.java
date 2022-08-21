67f426cddac4f246a52263b6ddef970352ddd89f
AltFantom <AltF4ntom@yandex.ru>
2022-08-21 16:42:21 +0300

    все работает, но тесты не проходит

--- src/main/java/org/example/GameOfLife.java
+++ src/main/java/org/example/GameOfLife.java
@@ -1,35 +1,79 @@
 package org.example;
 
-
 import java.io.*;
 
 public class GameOfLife {
   int iterator;
-  String[][] field = new String[50][50];
+  int x;
+  int y;
+  String[][] field;
 
   // TODO:
   public void game(String fileNameInput, String fileNameOutput) {
     readFileInput(fileNameInput);
     for (int i = 0; i < iterator; i++) {
-      //oneLifeCycle(field);
+      oneLifeCycle();
     }
     fillOutputFile(fileNameOutput);
   }
-  private void howManyTimesToDo(String firstStringInFile){
-    String[] array = firstStringInFile.split(",");
-    iterator = Integer.parseInt(array[2]);
+  private void oneLifeCycle(){
+    String [][] newField = new String[x][y];
+    int neighbors;
+    String element;
+    for (int i = 0; i <field.length ; i++) {
+      for (int j = 0; j <field[i].length; j++) {
+        neighbors = countNeighbor(i, j);
+        element = field[i][j];
+        if(isAlive(element)){
+          neighbors--;
+          if(neighbors < 2 || neighbors > 3){
+            element = "O";
+          }
+        }
+        else{
+          if(neighbors == 3){
+            element = "X";
+          }
+        }
+        newField[i][j] = element;
+      }
+    }
+    this.field = newField;
+  }
+  private int countNeighbor(int i, int j){
+    int count = 0;
+    for (int k = -1; k < 2; k++) {
+      for (int l = -1; l < 2; l++) {
+        if(field[(i + k + x) % x][(j + l + y) % y].equals("X")){
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+  private boolean isAlive(String element){
+    return element.equals("X");
+  }
+private void sizeFieldAndIterator(String line){
+  String[] array = line.split(",");
+  x = Integer.parseInt(array[0]);
+  y = Integer.parseInt(array[1]);
+  iterator = Integer.parseInt(array[2]);
+}
+  private void initializeArray(){
+    field = new String[x][y];
   }
   private void readFileInput(String fileNameInput){
     try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileNameInput))) {
-      howManyTimesToDo(bufferedReader.readLine());
+      sizeFieldAndIterator(bufferedReader.readLine());
+      initializeArray();
       int i = 0;
       while(bufferedReader.ready()){
         String[] line = bufferedReader.readLine().split(" ");
         int j = 0;
         for (String element :
                 line) {
-          if(element != null)
-            field[i][j++] = element;
+          field[i][j++] = element;
         }
         i++;
       }
@@ -38,15 +82,12 @@ public class GameOfLife {
       throw new RuntimeException(e);
     }
   }
-  private void oneLifeCycle(String[][] field){
-    String [][] newField = field;
-  }
   private void fillOutputFile(String file){
     try(FileWriter fileWriter = new FileWriter(file)) {
-      for (int i = 0; i < field.length; i++) {
-        for (int j = 0; j < field[i].length; j++) {
+      for (String[] strings : field) {
+        for (String string : strings) {
           String element;
-          if ((element = field[i][j]) != null)
+          if ((element = string) != null)
             fileWriter.write(element + " ");
         }
         fileWriter.write("\n");
