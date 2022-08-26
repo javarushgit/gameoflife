3ee65f63af019166695ca3c3cbdd405dca7e8232
Vladimir Semkin <vnsemkin@gmail.com>
2022-08-26 16:15:31 +0300

    version4(replace String array to char array, stole Ilja split method)

--- src/main/java/org/example/GameOfLife.java
+++ src/main/java/org/example/GameOfLife.java
@@ -4,104 +4,106 @@ import java.io.*;
 import java.util.Arrays;
 
 public class GameOfLife {
+  private char[][] initialGeneration;
+  private char[][] newGeneration;
+  private final String PATH = "src\\test\\resources\\";
+  private int axisY;
+  private int axisX;
+  private int cycleNumber;
+
   // TODO:
   public void game(String fileNameInput, String fileNameOutput)
   {
-    String[] inputData = readFieldParameters(fileNameInput);
-    int axisX = Integer.parseInt(inputData[0]);
-    int axisY = Integer.parseInt(inputData[1]);
-    int cycleNumber = Integer.parseInt(inputData[2]);
-    String[][] initialGeneration = readData(fileNameInput, axisX, axisY);
-    String[][] newGeneration = computeNewGeneration(initialGeneration, axisX, axisY, cycleNumber);
-    writeOutput(fileNameOutput, newGeneration);
+    readData(fileNameInput);
+    getNewGeneration();
+    writeOutput(fileNameOutput);
   }
-  public String[] readFieldParameters(String fileNameInput)
-  {
-    String[] inputData = new String[3];
-    try(BufferedReader bufferedReader = new BufferedReader(new FileReader(fileNameInput)))
-    {
-      inputData = bufferedReader.readLine().split(",");
-    }catch (IOException e)
-    {
-      e.printStackTrace();
-    }
-    return inputData;
-  }
-  public String[][] readData(String fileNameInput, int axisX, int axisY) {
-    String[][] initialGeneration = new String[axisX][axisY];
-    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileNameInput))) {
-      int count = -1;
-      while (bufferedReader.ready())
-      {
-        if(count ==-1)
-        {
-          bufferedReader.readLine();
-          count++;
-        }else {
-          String[] rowData = bufferedReader.readLine().split(" ");
-          System.arraycopy(rowData, 0, initialGeneration[count], 0, axisX);
-          count++;
-        }
+  private String[] splitSettings(String settings) {
+    String[] array = new String[3];
+    int begin = 0;
+    int index = 0;
+    for (int i = 0; i < settings.length(); i++) {
+      if (settings.charAt(i) == ',') {
+        array[index++] = settings.substring(begin, i);
+        begin = i + 1;
       }
-    } catch (IOException e) {
-      e.printStackTrace();
     }
-    return initialGeneration;
+    array[index] = settings.substring(begin);
+    return array;
   }
-  public String[][] computeNewGeneration(String[][] initialGeneration, int axisX, int axisY, int cycleNumber) {
+
+  public  void readData(String fileNameInput) {
+    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(PATH + fileNameInput))) {
+      String[] inputData = splitSettings(bufferedReader.readLine());
+      axisY = Integer.parseInt(inputData[0]);
+      axisX = Integer.parseInt(inputData[1]);
+      cycleNumber = Integer.parseInt(inputData[2]);
+      initialGeneration = new char[axisY][axisX];
+          for (int i = 0; i < axisY; i++) {
+            for (int j = 0; j < axisX; j++) {
+             initialGeneration[i][j] = (char) bufferedReader.read();
+             bufferedReader.read();
+            }
+          }
+      } catch (IOException e) {
+         throw new RuntimeException();
+    }
+  }
+  public void getNewGeneration() {
     int cycle = 0;
-    String[][] newGeneration = new String[axisX][axisY];
+    newGeneration = new char[axisY][axisX];
     while (cycle < cycleNumber) {
       cycle += 1;
-      for (int x = 0; x < axisX; x++) {
-        for (int y = 0; y < axisY; y++) {
+      for (int x = 0; x < axisY; x++) {
+        for (int y = 0; y < axisX; y++) {
           int neighbours = 0;
           int col;
           int row;
-          if (initialGeneration[x][y].equals("X")) {
+          if (initialGeneration[x][y] == ('X')) {
             neighbours -= 1;
           }
+          char aliveCell = 'X';
           for (int i = -1; i < 2; i++) {
             for (int j = -1; j < 2; j++) {
-              col = (x + i + axisX) % axisX;
-              row = (y + j + axisY) % axisY;
-              if (initialGeneration[col][row].equals("X")) {
+              col = (x + i + axisY) % axisY;
+              row = (y + j + axisX) % axisX;
+              if (initialGeneration[col][row] == aliveCell) {
                 neighbours++;
               }
             }
           }
-          if (initialGeneration[x][y].equals("X") && neighbours < 2) {
-            newGeneration[x][y] = "O";
-          } else if (initialGeneration[x][y].equals("X") && neighbours > 3) {
-            newGeneration[x][y] = "O";
-          } else if (initialGeneration[x][y].equals("O") && neighbours == 3) {
-            newGeneration[x][y] = "X";
+          char deadCell = 'O';
+          if (initialGeneration[x][y] == aliveCell && neighbours < 2) {
+            newGeneration[x][y] = deadCell;
+          } else if (initialGeneration[x][y] == aliveCell && neighbours > 3) {
+            newGeneration[x][y] = deadCell;
+          } else if (initialGeneration[x][y] == deadCell && neighbours == 3) {
+            newGeneration[x][y] = aliveCell;
           } else
             newGeneration[x][y] = initialGeneration[x][y];
         }
       }
-      for (int i = 0; i < axisX; i++) {
+      for (int i = 0; i < axisY; i++) {
         System.arraycopy(newGeneration[i], 0, initialGeneration[i], 0, initialGeneration[i].length);
       }
     }
-    return newGeneration;
   }
-  public void writeOutput(String fileNameOutput, String[][] newGeneration){
-      int lineCount = 0;
-      try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileNameOutput))){
-      for(String[] string: newGeneration) {
-        String stringOut = Arrays.toString(string)
-                .replaceAll("[][,]]", "")
-                .replaceAll("\\[", "");
-        if (lineCount == 0) {
-          bufferedWriter.write(stringOut);
-          lineCount++;
+  public void writeOutput(String fileNameOutput){
+      StringBuilder sb = new StringBuilder();
+      try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(PATH + fileNameOutput))){
+      for(char[] string: newGeneration) {
+        for (char c : string) {
+          if (c != ' ') {
+            sb.append(c).append(' ');
+          }
         }
-        else
-          bufferedWriter.write("\n"+stringOut);
+        sb.deleteCharAt(sb.lastIndexOf(" "));
+        sb.append('\n');
       }
-    }catch (IOException e){
-      e.printStackTrace();
+      sb.deleteCharAt(sb.lastIndexOf("\n"));
+      bufferedWriter.write(sb.toString());
+      }catch (IOException e){
+      throw new RuntimeException();
     }
   }
 }
